50e557dc81db6163ff5feaa0d17dd2a472da8654
Fabiola <alejandrafabiolalopez58@gmail.com>
2024-03-01 17:54:28 -0600

    1_8 React Server Components - use client

---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+
+export const Header = () => {
+
+    console.log( 'Homa mundo Header' )
+    return( <header>
+        <nav>
+          <ul>
+
+            <Link href="/">
+
+            <li>Home</li>
+          
+            </Link>
+
+            <Link href="/store">
+
+            <li>Store</li>
+
+            </Link>
+            
+          </ul>
+        </nav>
+      </header>)
+}
