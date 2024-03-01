301cfb5fdce7f916da986dbc42c040c7d36a6864
Fabiola <alejandrafabiolalopez58@gmail.com>
2024-03-01 17:08:55 -0600

    1_6 Como funciona la navegacion en next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from 'next/link'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,8 +22,19 @@ export default function RootLayout({
         <header>
           <nav>
             <ul>
+
+              <Link href="/">
+
               <li>Home</li>
+            
+              </Link>
+
+              <Link href="/store">
+
               <li>Store</li>
+
+              </Link>
+              
             </ul>
           </nav>
         </header>
