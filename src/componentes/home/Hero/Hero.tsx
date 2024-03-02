2e8a1c4073247ad37f39bf1d1b4233a082ad27d9
Fabiola <alejandrafabiolalopez58@gmail.com>
2024-03-01 19:45:17 -0600

    2_1 CSS Modules en Next

--- src/componentes/home/Hero/Hero.tsx
+++ src/componentes/home/Hero/Hero.tsx
@@ -1,9 +1,12 @@
+import styles from './Hero.module.css'
 export const Hero = () => {
 
     return(
-        <section>
+        <section className={styles.Hero} >
 
-            <h1>Hero</h1>
+            <h1>Future World</h1>
+
+            <h2>Empowering Your Tomorrow, Today!</h2>
 
         </section>
     )
