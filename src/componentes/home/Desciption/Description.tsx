829dcb57a58e5453e3c34838e0f583c4e514e69e
Fabiola <alejandrafabiolalopez58@gmail.com>
2024-03-01 21:10:24 -0600

    2_5 Manejo y optimizacion de imagenes

--- src/componentes/home/Desciption/Description.tsx
+++ src/componentes/home/Desciption/Description.tsx
@@ -1,13 +1,22 @@
+import styles from './Description.module.sass'
+import image from 'next/image'
 export const Description = () => {
 
     return(
-        <section>
+        <section className={styles.Description}>
 
-            <img src="/images/description.jpeg" alt="products" />
+            <img 
+            src="/images/description.jpeg" 
+            alt="products" 
+            width={500} 
+            height={300}         
+            />
 
+            <div className={styles.Description__text}>
             <h2>Description</h2>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae earum natus, ducimus a harum quae. Obcaecati ipsum vero aperiam quae deleniti perspiciatis cumque excepturi, distinctio sequi eveniet quasi ipsam repellendus!</p>
+            </div>
 
         </section>
     )
