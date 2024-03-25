c040289da51d120e3a617f7a399babbebd33f8bc
Fabiola <alejandrafabiolalopez58@gmail.com>
2024-03-25 15:00:48 -0600

    4_8 Proyecto - filtrando categorias de productos

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,27 +1,27 @@
 import { ProductsWrapper } from "app/componentes/Store/ProductsWrapper"
-import { getCollections } from "app/services/shopify/collections"
+import { getCollectionProducts, getCollections } from "app/services/shopify/collections"
 import { getProducts } from "app/services/shopify/products"
 
+interface CategoryProps {
+  params: {
+    categories: string[],
+  }
+  searchParams?: string
+}
 
-interface CategoryProps{
+export default async function Category(props: CategoryProps) {
+  const { categories } = props.params
+  let products = []
+  const collections = await getCollections()
+  
+  if (categories?.length > 0) {
+    const selectedCollectionId = collections.find((collection: any) => collection.handle === categories[0]).id
+    products = await getCollectionProducts(selectedCollectionId)
+  }else {
+    products = await getProducts()
+  }
 
-    params:{
-        categories: string[],
-    }
-        searhParams: string
-    }
-
-
-export default async function Category(props: CategoryProps){
-    
-    const products = await getProducts()
-    //const collections = await getCollections()
-
-    const { categories } = props.params
-    //throw new Error('Error: Boom!')
-    // Throw Error #2
-    // console.log(categories)
-    return(
-        <ProductsWrapper products={products}/>
-    )
+  return (
+    <ProductsWrapper products={products} />
+  )
 }
--- src/services/shopify/collections.ts
+++ src/services/shopify/collections.ts
@@ -5,7 +5,7 @@ export const getCollections = async () => {
 
     try{
 
-        const response = await fetch(shopifyUrls.products.all, {
+        const response = await fetch(shopifyUrls.collections.all, {
             headers: new Headers ({
                 'X-Shopify-Access-Token': env.SHOPIFY_TOKEN,
             })
@@ -24,4 +24,22 @@ export const getCollections = async () => {
         console.log(error)
     }
     
+}
+
+export const getCollectionProducts = async (id: string) => {
+    try{
+        const response = await fetch(shopifyUrls.collections.products(id), {  
+
+            headers: new Headers ({
+                'X-Shopify-Access-Token': env.SHOPIFY_TOKEN,
+            })
+        })
+        
+        const { products } = await response.json()
+        return products
+
+    } catch (error){
+      console.log(error)
+                   
+    }
 }
